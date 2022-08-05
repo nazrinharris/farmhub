068b083427547874550d0f38745a61d48f007ad6
nazrinharris <54095201+nazrinharris@users.noreply.github.com>
2022-08-05 21:42:24 +0800

    Super minor

--- lib/presentation/views/debug/playground_two_screen.dart
+++ lib/presentation/views/debug/playground_two_screen.dart
@@ -29,161 +29,170 @@ class PlaygroundTwoScreen extends StatelessWidget {
             return Scaffold(
               body: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  Center(
-                    child: PrimaryButton(
-                      width: 200,
-                      content: "Create Produce",
-                      onPressed: () {
-                        context.read<PlaygroundCubit>().createProduce(
-                              produceName: "Beijing Cabbage",
-                              currentProducePrice: 99,
-                            );
-                      },
-                    ),
-                  ),
-                  UIVerticalSpace6(),
-                  Center(
-                    child: PrimaryButton(
-                      width: 200,
-                      content: "Add New Price",
-                      onPressed: () {
-                        context.read<PlaygroundCubit>().addNewPrice(
-                              produceId: "M0EDrwfr8LGQc7EAQaR8",
-                              currentPrice: 21,
-                            );
-                      },
-                    ),
-                  ),
-                  UIVerticalSpace6(),
-                  Center(
-                    child: PrimaryButton(
-                      width: 200,
-                      content: "Run Debug",
-                      onPressed: () {
-                        context.read<PlaygroundCubit>().debugMethod(
-                              produceId: "M0EDrwfr8LGQc7EAQaR8",
-                            );
-                      },
-                    ),
-                  ),
-                  UIVerticalSpace24(),
-                  if (state is Error)
-                    Container(
-                      child: Text(state.failure.toString()),
-                    ),
-                  if (state is GetPricesCompleted) SizedBox.shrink(),
-                  UIVerticalSpace14(),
-                  Center(
-                    child: PrimaryButton(
-                      width: 200,
-                      content: "Get Two Weeks",
-                      onPressed: () {
-                        context
-                            .read<PlaygroundCubit>()
-                            .getTwoWeeksPrices(produceId: "3Wwn4LGLbbgtftX7MDNs");
-                      },
-                    ),
-                  ),
-                  UIVerticalSpace14(),
-                  Center(
-                    child: PrimaryButton(
-                      width: 200,
-                      content: "Create Random",
-                      onPressed: () {
-                        context.read<PlaygroundCubit>().createMorePrices(
-                              pricesAmount: 365,
-                              produceId: "oGItJKKddeuyv6Z1fzDC",
-                            );
-                      },
-                    ),
-                  ),
-                  UIVerticalSpace14(),
-                  Center(
-                    child: PrimaryButton(
-                      width: 200,
-                      content: "Get Produce",
-                      onPressed: () {
-                        context
-                            .read<PlaygroundCubit>()
-                            .retrieveProduce(produceId: "WBOIwlizkFZkaokHv7OK");
-                      },
-                    ),
-                  ),
-                  UIVerticalSpace14(),
-                  Center(
-                    child: PrimaryButton(
-                      width: 200,
-                      content: "Get Price",
-                      onPressed: () {
-                        context.read<PlaygroundCubit>().getPrice(
-                              produceId: "WBOIwlizkFZkaokHv7OK",
-                              priceId: "VnvECcHslrAsOEiWnGwc",
-                            );
-                      },
-                    ),
-                  ),
-                  UIVerticalSpace14(),
-                  Center(
-                    child: PrimaryButton(
-                      width: 200,
-                      content: "Delete subPrice",
-                      onPressed: () {
-                        context.read<PlaygroundCubit>().deletePriceFromAggregate(
-                              produceId: "4pfZr3GSkFilgggKlowp",
-                              chosenDate: "22-05-2022",
-                              chosenYear: "2022",
-                            );
-                      },
-                    ),
-                  ),
-                  UIVerticalSpace14(),
-                  Center(
-                    child: PrimaryButton(
-                      width: 200,
-                      content: "Add to Fav",
-                      onPressed: () async {
-                        await repository
-                            .addToFavorites(user, "FS4MgEEIXsDHKS0gR0ih")
-                            .then((foldThis) {
-                          foldThis.fold(
-                            (l) => print(l),
-                            (r) => null,
-                          );
-
-                          FarmhubUser user = context.read<GlobalAuthCubit>().state.farmhubUser!;
-
-                          print("After adding favorite");
-                          print(user.produceFavoritesList);
-                        });
-                      },
-                    ),
-                  ),
-                  UIVerticalSpace14(),
-                  Center(
-                    child: PrimaryButton(
-                      width: 200,
-                      content: "Remove from Fav",
-                      onPressed: () async {
-                        await repository
-                            .removeFromFavorites(user, "FS4MgEEIXsDHKS0gR0ih")
-                            .then((foldThis) {
-                          foldThis.fold(
-                            (f) => print(f),
-                            (r) => null,
-                          );
-
-                          FarmhubUser user = context.read<GlobalAuthCubit>().state.farmhubUser!;
-
-                          print("After removing favorite");
-                          print(user.produceFavoritesList);
-                        });
-                      },
-                    ),
+                  PrimaryButton(
+                    content: "Send Verification",
+                    onPressed: () {},
                   ),
                 ],
               ),
+              // body: Column(
+              //   mainAxisAlignment: MainAxisAlignment.center,
+              //   crossAxisAlignment: CrossAxisAlignment.center,
+              //   children: [
+              //     Center(
+              //       child: PrimaryButton(
+              //         width: 200,
+              //         content: "Create Produce",
+              //         onPressed: () {
+              //           context.read<PlaygroundCubit>().createProduce(
+              //                 produceName: "Beijing Cabbage",
+              //                 currentProducePrice: 99,
+              //               );
+              //         },
+              //       ),
+              //     ),
+              //     UIVerticalSpace6(),
+              //     Center(
+              //       child: PrimaryButton(
+              //         width: 200,
+              //         content: "Add New Price",
+              //         onPressed: () {
+              //           context.read<PlaygroundCubit>().addNewPrice(
+              //                 produceId: "M0EDrwfr8LGQc7EAQaR8",
+              //                 currentPrice: 21,
+              //               );
+              //         },
+              //       ),
+              //     ),
+              //     UIVerticalSpace6(),
+              //     Center(
+              //       child: PrimaryButton(
+              //         width: 200,
+              //         content: "Run Debug",
+              //         onPressed: () {
+              //           context.read<PlaygroundCubit>().debugMethod(
+              //                 produceId: "M0EDrwfr8LGQc7EAQaR8",
+              //               );
+              //         },
+              //       ),
+              //     ),
+              //     UIVerticalSpace24(),
+              //     if (state is Error)
+              //       Container(
+              //         child: Text(state.failure.toString()),
+              //       ),
+              //     if (state is GetPricesCompleted) SizedBox.shrink(),
+              //     UIVerticalSpace14(),
+              //     Center(
+              //       child: PrimaryButton(
+              //         width: 200,
+              //         content: "Get Two Weeks",
+              //         onPressed: () {
+              //           context
+              //               .read<PlaygroundCubit>()
+              //               .getTwoWeeksPrices(produceId: "3Wwn4LGLbbgtftX7MDNs");
+              //         },
+              //       ),
+              //     ),
+              //     UIVerticalSpace14(),
+              //     Center(
+              //       child: PrimaryButton(
+              //         width: 200,
+              //         content: "Create Random",
+              //         onPressed: () {
+              //           context.read<PlaygroundCubit>().createMorePrices(
+              //                 pricesAmount: 365,
+              //                 produceId: "oGItJKKddeuyv6Z1fzDC",
+              //               );
+              //         },
+              //       ),
+              //     ),
+              //     UIVerticalSpace14(),
+              //     Center(
+              //       child: PrimaryButton(
+              //         width: 200,
+              //         content: "Get Produce",
+              //         onPressed: () {
+              //           context
+              //               .read<PlaygroundCubit>()
+              //               .retrieveProduce(produceId: "WBOIwlizkFZkaokHv7OK");
+              //         },
+              //       ),
+              //     ),
+              //     UIVerticalSpace14(),
+              //     Center(
+              //       child: PrimaryButton(
+              //         width: 200,
+              //         content: "Get Price",
+              //         onPressed: () {
+              //           context.read<PlaygroundCubit>().getPrice(
+              //                 produceId: "WBOIwlizkFZkaokHv7OK",
+              //                 priceId: "VnvECcHslrAsOEiWnGwc",
+              //               );
+              //         },
+              //       ),
+              //     ),
+              //     UIVerticalSpace14(),
+              //     Center(
+              //       child: PrimaryButton(
+              //         width: 200,
+              //         content: "Delete subPrice",
+              //         onPressed: () {
+              //           context.read<PlaygroundCubit>().deletePriceFromAggregate(
+              //                 produceId: "4pfZr3GSkFilgggKlowp",
+              //                 chosenDate: "22-05-2022",
+              //                 chosenYear: "2022",
+              //               );
+              //         },
+              //       ),
+              //     ),
+              //     UIVerticalSpace14(),
+              //     Center(
+              //       child: PrimaryButton(
+              //         width: 200,
+              //         content: "Add to Fav",
+              //         onPressed: () async {
+              //           await repository
+              //               .addToFavorites(user, "FS4MgEEIXsDHKS0gR0ih")
+              //               .then((foldThis) {
+              //             foldThis.fold(
+              //               (l) => print(l),
+              //               (r) => null,
+              //             );
+
+              //             FarmhubUser user = context.read<GlobalAuthCubit>().state.farmhubUser!;
+
+              //             print("After adding favorite");
+              //             print(user.produceFavoritesList);
+              //           });
+              //         },
+              //       ),
+              //     ),
+              //     UIVerticalSpace14(),
+              //     Center(
+              //       child: PrimaryButton(
+              //         width: 200,
+              //         content: "Remove from Fav",
+              //         onPressed: () async {
+              //           await repository
+              //               .removeFromFavorites(user, "FS4MgEEIXsDHKS0gR0ih")
+              //               .then((foldThis) {
+              //             foldThis.fold(
+              //               (f) => print(f),
+              //               (r) => null,
+              //             );
+
+              //             FarmhubUser user = context.read<GlobalAuthCubit>().state.farmhubUser!;
+
+              //             print("After removing favorite");
+              //             print(user.produceFavoritesList);
+              //           });
+              //         },
+              //       ),
+              //     ),
+              //   ],
+              // ),
             );
           },
         );
