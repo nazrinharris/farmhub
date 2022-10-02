c4c59c25d45faa4256b0b66eba3e0e5edb61d9dc
nazrinharris <54095201+nazrinharris@users.noreply.github.com>
2022-10-02 15:42:36 -0400

    Fixed #134

--- lib/presentation/smart_widgets/produce_dialogs/app_dialogs.dart
+++ lib/presentation/smart_widgets/produce_dialogs/app_dialogs.dart
@@ -636,7 +636,8 @@ NAlertDialog returnResetPasswordConfirmation(
               textAlign: TextAlign.center,
             ),
           ),
-          Padding(
+          Container(
+            alignment: Alignment.center,
             padding: const EdgeInsets.only(top: 14),
             child: Text(
               "Click the link to reset your password.",
