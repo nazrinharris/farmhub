fcf7fef6fe7f438916b9cd5f8ff5bd9c104bd52e
nazrinharris <54095201+nazrinharris@users.noreply.github.com>
2023-01-06 08:18:27 -0500

    Init

--- lib/main.dart
+++ lib/main.dart
@@ -38,6 +38,7 @@ Future<void> main() async {
   // Comment for farm and account types
   // Comment for phone sign-in backend
   // Comment for aggregate prices fix
+  // Comment for flutter 3 migration
 }
 
 class FarmhubApp extends StatelessWidget {
