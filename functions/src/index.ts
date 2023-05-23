ff3f4e33218cf6937565e932f5eb25f5d6b98e7b
nazrinharris <54095201+nazrinharris@users.noreply.github.com>
2023-05-23 18:16:21 +0800

    Node stuff
    ESLint and stuff

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,13 +2,11 @@ import * as functions from "firebase-functions";
 import * as admin from "firebase-admin";
 admin.initializeApp();
 
-
-
 export const setAppVersion = functions
   .region("asia-southeast1")
   .https.onCall(async (data, context) => {
     const uid = context.auth?.uid;
-    const appVersion = data.appVersion; 
+    const appVersion = data.appVersion;
 
     if (!uid) {
       throw new functions.https.HttpsError(
@@ -24,7 +22,49 @@ export const setAppVersion = functions
       );
     }
 
-    await admin.auth().setCustomUserClaims(uid, {appVersion});
+    await admin.auth().setCustomUserClaims(uid, { appVersion });
 
-    return {success: true};
+    return { success: true };
+  });
+
+export const renameAggregatePrices = functions
+  .region("asia-southeast1")
+  .https.onRequest(async (req, res) => {
+    const db = admin.firestore();
+
+    // Get all documents in the "produce" collection
+    const produceRef = db.collection("produce");
+    const produceSnapshot = await produceRef.get();
+
+    // For each document in the "produce" collection
+    produceSnapshot.forEach((doc) => {
+      const oldDocRef = doc.ref
+        .collection("aggregatePrices")
+        .doc("aggregatePrices2023");
+      const newDocRef = doc.ref
+        .collection("aggregatePrices")
+        .doc("aggregatePrices");
+
+      db.runTransaction(async (transaction) => {
+        const oldDoc = await transaction.get(oldDocRef);
+        const newDoc = await transaction.get(newDocRef);
+
+        // If the "aggregatePrices2022" document exists and the "aggregatePrices" document does not exist
+        if (oldDoc.exists && !newDoc.exists) {
+          // Copy data from old document to new document
+          transaction.set(newDocRef, oldDoc.data());
+
+          // Delete old document
+          transaction.delete(oldDocRef);
+        }
+      })
+        .then(() => {
+          console.log("Document successfully updated!");
+        })
+        .catch((error) => {
+          console.error("Error updating document: ", error);
+        });
+    });
+
+    res.send("Operation completed");
   });
